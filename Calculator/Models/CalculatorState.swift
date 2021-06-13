738f2e4009d0c5052176fcf62cdf1d196a6ca3fe
Johan Wejdenstolpe <johan.wejdenstolpe@bredband.net>
2021-06-13 09:58:57 +0200

    Add calculator view model

---
+++ Calculator/Models/CalculatorState.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorState.swift
+//  Calculator
+//
+//  Created by Johan Wejdenstolpe on 2021-06-12.
+//
+
+import Foundation
+
+enum CalculatorState {
+    case advanced
+    case basic
+}
