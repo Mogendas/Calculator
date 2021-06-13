f82469ba07a8b0a70ab71a62971573415f9ae1a6
Johan Wejdenstolpe <johan.wejdenstolpe@bredband.net>
2021-06-13 15:08:54 +0200

    Add calculator

--- Calculator/ViewModels/CalculatorViewModel.swift
+++ Calculator/ViewModels/CalculatorViewModel.swift
@@ -12,12 +12,19 @@ typealias CalculationClosure = ((String) -> Void)?
 protocol CalculatorViewModel {
     var calculationClosure: CalculationClosure { get set }
     var state: CalculatorState { get set }
+    var calculator: Calculator { get set }
+    
     func buttonPressed(button: UIButton)
 }
 
 final class DefaultCalculatorViewModel: CalculatorViewModel {
     var calculationClosure: ((String) -> Void)?
     var state: CalculatorState = .basic
+    var calculator: Calculator
+    
+    init(calculator: Calculator) {
+        self.calculator = calculator
+    }
     
     func buttonPressed(button: UIButton) {
         
